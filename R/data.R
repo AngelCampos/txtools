3342421c4e79b6c57b04622a6756f9f2f32aab36
Angel García-Campos <xxwildfangxx@gmail.com>
2020-02-12 20:10:35 +0200

    additional data and resp docs

---
+++ R/data.R
@@ -0,0 +1,23 @@
+# Extended IUPAC code for
+Biostrings::IUPAC_CODE_MAP
+
+#' IUPAC code extended to consider deletions
+#'
+#' The IUPAC_CODE_MAP_extended named character vector contains the mapping from
+#' the IUPAC nucleotide ambiguity codes to their meaning adding the deletion
+#' code "-", this is required for computing the consensus strings between
+#' overlapping reads.
+#'
+#' @format Character vector
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source Adapted from Biostrings::IUPAC_CODE_MAP
+"IUPAC_CODE_MAP_extended"
+
+"IUPAC_code_2nucs"
+
+
+"IUPAC_code_simpl"
