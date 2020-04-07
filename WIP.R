5576f8a7d9a98a56c0c0f9da804c986c6f3127f3
Angel García-Campos <xxwildfangxx@gmail.com>
2020-04-07 12:52:19 -0500

    updatin WIP

--- WIP.R
+++ WIP.R
@@ -1,16 +1,43 @@
 tx_genCoorTab(txReads, geneAnnot)
 
-
 genome <- BSgenome.Hsapiens.UCSC.hg19::BSgenome.Hsapiens.UCSC.hg19
-genome
-tx_addRefSeqDT <- function(DT, fastaGenome){
-    if(class(DT) == "list"){
-        if(sum(class(DT[[1]]) == "data.table") == 0){
-            stop("x must be of data.table class")
-        }
+fastaGenome <- genome
+DT <- resTab3
 
-    }
+
+
+
+
+tx_addRefSeqDT(resTab3, genome, geneAnnot)
+
+
+tmpA <- geneAnnot[gene,]$blockStarts %>% strsplit(split = ",") %>% unlist() %>% as.numeric()
+tmpB <- geneAnnot[gene,]$blockSizes  %>% strsplit(split = ",") %>% unlist() %>% as.numeric()
+iBlocks <- (sapply(1:length(tmpA), function(i){
+    c(tmpA[i],(tmpA[i] + tmpB[i] -1))
+}) %>% t) + geneAnnot[gene,]$start
+
+getGenSeqInGenome <- function(gene, geneAnnot, genome){
+    tmpA <- geneAnnot[gene,]$blockStarts %>% strsplit(split = ",") %>% unlist() %>% as.numeric()
+    tmpB <- geneAnnot[gene,]$blockSizes  %>% strsplit(split = ",") %>% unlist() %>% as.numeric()
+    iBlocks <- (sapply(1:length(tmpA), function(i){
+        c(tmpA[i],(tmpA[i] + tmpB[i] -1))
+    }) %>% t) + geneAnnot[gene,]$start
+    if(geneAnnot[gene,]$end %in% as.vector(iBlocks)){
+        if(geneAnnot[gene,]$strand == "+"){
+            sapply(1:nrow(iBlocks), function(k){
+                str_sub(genome[[geneAnnot[gene,]$chr]], iBlocks[k, 1], iBlocks[k,2])
+            }) %>% paste(collapse = "") %>% DNAString()
+        }else if(geneAnnot[gene,]$strand == "-"){
+            sapply(1:nrow(iBlocks), function(k){
+                str_sub(genome[[geneAnnot[gene,]$chr]], iBlocks[k, 1], iBlocks[k,2])
+            }) %>% paste(collapse = "") %>% DNAString() %>% reverseComplement()
+        }
+    }else{stop(paste("Malformed exon structure at gene", gene))}
 }
+
+
+i <- 1
 resTab <- tx_covNucFreqDT(txReads, geneAnnot)
 DT <- resTab$uc003lam.1
 sapply(DT, class)
@@ -33,4 +60,3 @@ DT$uc010nap.1 %>% str
 
 
 do.call(resTab, what = rbind) %>% View
-)
