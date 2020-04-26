803f24dcf8b1ab3a41db9250e5e2f20298b9c2c0
Angel García-Campos <xxwildfangxx@gmail.com>
2020-04-26 15:12:26 -0500

    update tests

--- tests/testthat/test-txTools.R
+++ tests/testthat/test-txTools.R
@@ -1,2 +1,14 @@
-# testing function
-#test_that(expect_identical())
+# Tests
+library(testthat)
+
+# Loading demo data
+library(txtools)
+bamFile <- system.file("extdata", "example_hg19.bam", package = "txtools")
+bedFile <- system.file("extdata", "twoUCSCgenes_hg19.bed", package = "txtools")
+reads <- txtools::tx_load_bam(bamFile, loadSeq = T, verbose = T, yieldSize = 10000)
+geneAnnot <- txtools::tx_load_bed(bedFile) # plyranges read_bed function
+txReads <- txtools::tx_reads(reads, geneAnnot, withSeq = T, verbose = T)
+# Length
+testthat::expect_identical(length(txReads), 2L)
+# Resulting class
+testthat::expect_identical(as.character(class(txReads)), "CompressedGRangesList")
