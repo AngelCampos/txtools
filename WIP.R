3bd447922d55e31e64089b3aa2b77355393bf5ed
Angel García-Campos <xxwildfangxx@gmail.com>
2020-05-13 18:52:18 -0500

    uptick version

--- WIP.R
+++ WIP.R
@@ -1,14 +1,3 @@
-
-# Function to merge DT objects into one DT
-# tmp <- do.call(resTab3, what = rbind)
-
-# Function to split DT objects by gene
-# tmp2 <- split(tmp, tmp$gene)
-
-# Function to load genomes from fasta files
-#
-
-
 # TODO:
 # - Functions examples
 # - Results plotting functions
