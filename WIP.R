b74f588bfdc77e23c70dc46fc25c652c89554772
Angel García-Campos <xxwildfangxx@gmail.com>
2020-05-13 19:44:50 -0500

    updated WIP

--- WIP.R
+++ WIP.R
@@ -1,4 +1,11 @@
+# Proposed functions:
+# - tx_add_mismatchCountsRatio
+# - tx_add_mismatchCountsRatio
+#   Calculate the number of mismatched nucleotide reads. Both in counts and in ratio.
+
 # TODO:
+# - Badges with: https://github.com/GuangchuangYu/badger
+# - Hex logo with: https://github.com/GuangchuangYu/hexSticker
 # - Functions examples
 # - Results plotting functions
 # - Meta-analysis functions
