19435344295c418a90962a4fbaf66c48b2781ec2
Miguel Garcia <angel.campos@ciencias.unam.mx>
2022-04-01 15:45:56 -0600

    deprecate _diffNucToRef functions, to call them _misincorp...

--- R/deprecated.R
+++ R/deprecated.R
@@ -27,3 +27,52 @@ tx_reads_mc <- function(reads, geneAnnot, minReads = 50, withSeq = FALSE, verbos
     .Deprecated("tx_reads")
     tx_reads(reads, geneAnnot, minReads, withSeq, verbose, nCores)
 }
+
+
+#' Add number of nucleotide reads different to the reference genome
+#'
+#' Add a column to DT of the sum of nucleotide frequency different to the
+#' reference sequence counting deletions, without considering 'N's nor inserts
+#' '.' into the calculation.
+#'
+#' @param DT data.table. A data.table object. See
+#' \code{\link{tx_makeDT_coverage}}, \code{\link{tx_makeDT_nucFreq}} or
+#' \code{\link{tx_makeDT_covNucFreq}} functions.
+#'
+#' @return data.table
+#' @export
+tx_add_diffNucToRef <- function(DT){
+    lifecycle::deprecate_warn("0.0.7", "tx_add_diffNucToRef()", "tx_add_misincCount()")
+    DT <- check_DT(DT) %>% hlp_removeColumnIfPresent("diffToRef")
+    selNucs <- setdiff(intersect(txtools::IUPAC_code_2nucs, names(DT)), c(".", "N"))
+    tmp <- DT[, selNucs, with = FALSE]
+    OUT <- rep(NA, nrow(DT))
+    nucsInRef <- unique(DT$refSeq)
+    for(i in nucsInRef){
+        selNucs <- setdiff(c("A", "C", "G", "T", "-"), i)
+        OUT[which(DT$refSeq == i)] <- rowSums(tmp[which(DT$refSeq == i), selNucs, with = F])
+    }
+    tibble::add_column(DT, diffToRef = OUT)
+}
+
+#' Add different nucleotide reads to total ratio
+#'
+#' Add a column to DT of the ratio of different nucleotides to the total of
+#' meaningfuk nucleotide reads not counting undetermined 'N' and inserts '.'.
+#'
+#' @param DT data.table
+#' @param addDiffandTotalCols Set to TRUE to add counts of total nucleotides
+#' read (nucTotal) and different to reference nucleotide (diffToRef) columns.
+#'
+#' @return data.table
+#' @export
+#'
+#' @seealso \code{\link{tx_add_diffNucToRef}} and \code{\link{tx_add_nucTotal}}
+tx_add_diffNucToRefRatio <- function(DT, addDiffandTotalCols = FALSE){
+    lifecycle::deprecate_warn("0.0.7", "tx_add_diffNucToRefRatio()", "tx_add_misincRate()")
+    DT <- check_DT(DT) %>% hlp_removeColumnIfPresent("diffToRefRatio")
+    tmpDT <- tx_add_diffNucToRef(DT) %>% tx_add_nucTotal()
+    tmp <- round(tmpDT$diffToRef / tmpDT$nucTotal, 6)
+    if(addDiffandTotalCols){DT <- tmpDT}
+    tibble::add_column(DT, diffToRefRatio = tmp)
+}
